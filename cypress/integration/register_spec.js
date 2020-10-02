821f2860d852331cca0d96964907a2ab7320344b
Zi Ye <zi.ye1@thoughtworks.com>
2020-10-02 09:28:52 +0800

    add data-test-id param to register test

--- cypress/integration/register_spec.js
+++ cypress/integration/register_spec.js
@@ -13,25 +13,24 @@ describe('register', () => {
       cy.get('input[name=username]').type(username);
       cy.get('input[name=password]').type(password);
 
-      cy.contains('button', 'Register').click();
+      cy.get('[data-test-id="register"]').click();
 
       cy.url().should('include', '/login');
-      cy.get('#root').contains('Registration successful');
+      cy.get('[data-test-id="alert"]').contains('Registration successful');
     });
   });
 
   it('should registered failed when register given all fields are empty', () => {
-    cy.contains('button', 'Register').click();
+    cy.get('[data-test-id="register"]').click();
 
-    cy.url().should('include', '/register');
-    cy.get('#root').contains('First Name is required');
-    cy.get('#root').contains('Last Name is required');
-    cy.get('#root').contains('Username is required');
-    cy.get('#root').contains('Password is required');
+    cy.get('[data-test-id="form"]').contains('First Name is required');
+    cy.get('[data-test-id="form"]').contains('Last Name is required');
+    cy.get('[data-test-id="form"]').contains('Username is required');
+    cy.get('[data-test-id="form"]').contains('Password is required');
   });
 
   it('should jump to login page when click Cancel link given stay in register page', () => {
-    cy.contains('a', 'Cancel').click();
+    cy.get('[data-test-id="cancel"]').click();
 
     cy.url().should('include', '/login');
   });
--- src/app/RegisterPage/RegisterPage.jsx
+++ src/app/RegisterPage/RegisterPage.jsx
@@ -49,7 +49,7 @@ class RegisterPage extends React.Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
+                <form name="form" onSubmit={this.handleSubmit} data-test-id={'form'}>
                     <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
                         <label htmlFor="firstName">First Name</label>
                         <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
@@ -79,11 +79,11 @@ class RegisterPage extends React.Component {
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        {registering && 
+                        <button className="btn btn-primary" data-test-id={'register'}>Register</button>
+                        {registering &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
-                        <Link to="/login" className="btn btn-link">Cancel</Link>
+                        <Link to="/login" className="btn btn-link" data-test-id={'cancel'}>Cancel</Link>
                     </div>
                 </form>
             </div>
