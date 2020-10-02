eded992a17fbced646b1620f90b2eb747f386789
Zi Ye <zi.ye1@thoughtworks.com>
2020-10-02 09:49:36 +0800

    add data-test-id to login page test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -22,10 +22,10 @@ describe('login', () => {
       cy.get('input[name=username]').type(username);
       cy.get('input[name=password]').type(password);
 
-      cy.contains('button', 'Login').click();
+      cy.get('[data-test-id="login"]').click();
 
       cy.url().should('include', '/');
-      cy.get('#root').contains(`Hi ${firstName}!`);
+      cy.get('[data-test-id="tip"]').contains(`Hi ${firstName}!`);
     });
   });
 
@@ -33,10 +33,9 @@ describe('login', () => {
     cy.get('input[name=username]').type('unregistered account');
     cy.get('input[name=password]').type('123456');
 
-    cy.contains('button', 'Login').click();
+    cy.get('[data-test-id="login"]').click();
 
-    cy.url().should('include', '/login');
-    cy.get('#root').contains('Username or password is incorrect');
+    cy.get('[data-test-id="alert"]').contains('Username or password is incorrect');
   });
 
   it('should login failed when login given a registered account with error password', () => {
@@ -45,15 +44,21 @@ describe('login', () => {
       cy.get('input[name=username]').type(username);
       cy.get('input[name=password]').type('234567');
 
-      cy.contains('button', 'Login').click();
+      cy.get('[data-test-id="login"]').click();
 
-      cy.url().should('include', '/login');
-      cy.get('#root').contains('Username or password is incorrect');
+      cy.get('[data-test-id="alert"]').contains('Username or password is incorrect');
     });
   });
 
+  it('should show error message when login given all fields are empty', () => {
+    cy.get('[data-test-id="login"]').click();
+
+    cy.get('[data-test-id="form"]').contains('Username is required');
+    cy.get('[data-test-id="form"]').contains('Password is required');
+  });
+
   it('should jump to register page when click register link given stay in login page', () => {
-    cy.contains('a', 'Register').click();
+    cy.get('[data-test-id="register"]').click();
 
     cy.url().should('include', '/register');
   });
